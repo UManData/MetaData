e5274f063771f3c4708cfddb36e8d034b873392a
Argon <jirhiker@gmail.com>
2019-03-06 09:30:56 -0600

    repo updated for analysis 1810-01

---
+++ scripts/argus/argus_sample.py
@@ -0,0 +1,105 @@
+#!Measurement
+'''
+baseline:
+  after: true
+  before: false
+  counts: 60
+  detector: H1
+  mass: 39.2
+  settling_time: 10.0
+default_fits: nominal
+equilibration:
+  eqtime: 20.0
+  inlet: C
+  inlet_delay: 3
+  outlet: E
+  use_extraction_eqtime: false
+multicollect:
+  counts: 150
+  detector: H1
+  isotope: Ar40
+peakcenter:
+  after: true
+  before: false
+  detector: H1
+  detectors:
+  - H1
+  - AX
+  - CDD
+  integration_time: 0.262144
+  isotope: Ar40
+peakhop:
+  generate_ic_table: false
+  hops_name: hops_39_on_CDD
+  ncycles: 0
+  use_peak_hop: false
+'''
+
+ACTIVE_DETECTORS=('H1','AX','L1','L2', 'CDD')
+
+def main():
+    #this is a comment
+    '''
+        this is a multiline
+        comment aka docstring
+    '''
+    #display information with info(msg)
+    info('unknown measurement script')
+
+    #set the spectrometer parameters
+    #provide a value
+    #set_source_parameters(YSymmetry=10)
+
+    #or leave blank and values are loaded from a config file (setupfiles/spectrometer/config.cfg)
+    #set_source_optics()
+
+    #set the cdd operating voltage
+    #set_cdd_operating_voltage(100)
+
+    if mx.peakcenter.before:
+        peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope)
+
+    #open a plot panel for this detectors
+    activate_detectors(*ACTIVE_DETECTORS)
+
+    if mx.baseline.before:
+        baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector)
+
+    #position mass spectrometer
+    position_magnet(mx.multicollect.isotope, detector=mx.multicollect.detector)
+
+    #gas is staged behind inlet
+
+    #post equilibration script triggered after eqtime elapsed
+    #equilibrate is non blocking
+    #so use either a sniff of sleep as a placeholder until eq finished
+    if mx.equilibration.use_extraction_eqtime:
+        e = ex.eqtime
+    else:
+        e = mx.equilibration.eqtime
+
+    equilibrate(eqtime=e, inlet=mx.equilibration.inlet, 
+    delay=mx.equilibration.inlet_delay,
+    outlet=mx.equilibration.outlet)
+
+    #equilibrate returns immediately after the inlet opens
+    set_time_zero()
+
+    sniff(e)
+    #set default regression
+    set_fits()
+    set_baseline_fits()
+
+    #multicollect on active detectors
+    multicollect(ncounts=mx.multicollect.counts, integration_time=1)
+
+    if mx.baseline.after:
+        baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector)
+
+    if mx.peakcenter.after:
+        activate_detectors(*mx.peakcenter.detectors, **{'peak_center':True})
+        peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope, 
+                    integration_time=mx.peakcenter.integration_time)
+    info('finished measure script')
+
+#========================EOF==============================================================
