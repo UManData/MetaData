7431cc993c04562d54ee7115b605404fce76179d
Argon <jirhiker@gmail.com>
2019-04-04 10:02:09 -0500

    repo updated for analysis ba-01-A-13743

---
+++ scripts/argus/argus_EXCIMER_blank_pump_to_IP.py
@@ -0,0 +1,104 @@
+#===============================================================================
+# EXTRACTION SCRIPT argus_EXCIMER_blank_pump_to_IP.py
+#===============================================================================
+"""
+"""
+
+def main():
+    info('EXCIMER sample')
+    
+    # prepare for analysis
+    close(description='Prep IG')
+    
+    if analysis_type=='blank':
+        info('is blank. not heating')
+        open('B') # close CO2 inlet
+        sleep(2)
+        close('D') # Ion Pump
+        sleep(2)
+        sleep(196) # Ablation time (76s) + cleaning time (120s)
+    else:
+        info('move to position {}'.format(position))
+        move_to_position(position)
+        
+        # prepare for extraction
+        close('B') # close CO2 inlet
+        sleep(2)
+        close('D') # Ion Pump
+        sleep(2)
+        enable()
+        fire_laser()
+        if ramp_duration>0:
+            '''
+            style 1.
+            '''
+            begin_interval(duration)
+            info('ramping to {}. ramp_duration={}'.format(extract_value, ramp_duration))
+            ramp(setpoint=extract_value, duration=ramp_duration)
+            complete_interval()
+
+        else:
+            extract()
+            sleep(duration)
+            
+    end_extract()
+    disable()
+    
+    close(description='MS IG')
+    sleep(cleanup)
+#===============================================================================
+# POST EQUILIBRATION SCRIPT argus_pump_sample_to_IP.py
+#===============================================================================
+"""
+"""
+
+# '---Equilibrate gas into mass spectrometer
+# '---First, if overlapping runs, make sure spectrometer
+# '---has been pumped down for the minimum time specified
+# IF OverlapRuns
+#     BeginInterval RemainingSpecPumpdown
+#         Message "Waiting for minimum spectrometer pumpdown--"
+#     CompleteInterval
+# END if
+# '---Now equilibrate gas from extraction line into MS
+# Close "MS IG"
+# BeginInterval 10
+# CompleteInterval
+# Close MSIonPump    '---Close Spec/Ion Pump valve
+# Delay 3
+# Open ExtrToMS    '---Open spec/extr. line valve
+# Message "Allowing equilibration--"
+# Equil    '---Doing gas equilibration subroutine
+# '---End of equilibration, set valves for analysis
+# Close ExtrToMS             '---Close spec/Getter valve
+# Delay 2
+# Open "Inlet 1"
+# Open "Inlet 2"
+# Open "Prep IG"
+# '---Valves set for analysis, measure the gas
+# MeasureGas
+# Delay 2
+# Open MSIonPump
+# Open "MS IG"
+
+
+def main():
+    info('pump sample')
+    open(description='CO2 Inlet')
+    open(description='Excimer Inlet')
+    close(description='Prep IG')
+    open(description='Ion Pump')
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT argus_pump_ms.py
+#===============================================================================
+"""
+"""
+
+# Open "MS IG"
+
+
+def main():
+    info('pump ms')
+    open(description='MSIonPump')
+    close(description='MS IG')
