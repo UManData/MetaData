5c9d3402a03b414ea507f48f9ce6cc97dc9437c0
Argon <jirhiker@gmail.com>
2021-09-05 15:15:47 -0500

    repo updated for analysis ba-01-A-20453

--- scripts/argus/argus_Air_(no_split)_pump_to_IP.py
+++ scripts/argus/argus_Air_(no_split)_pump_to_IP.py
@@ -58,3 +58,58 @@ def main():
     #close('E')
     #sleep(2)
     #open('C')
+#===============================================================================
+# POST EQUILIBRATION SCRIPT argus_pump_air_to_IP.py
+#===============================================================================
+"""
+"""
+
+# '---Equilibrate gas into mass spectrometer
+# '---First, if overlapping runs, make sure spectrometer
+# '---has been pumped down for the minimum time specified
+# IF OverlapRuns
+#     BeginInterval RemainingSpecPumpdown
+#         Message "Waiting for minimum spectrometer pumpdown--"
+#     CompleteInterval
+# END if
+# '---Now equilibrate gas from extraction line into MS
+# Close "MS IG"
+# BeginInterval 10
+# CompleteInterval
+# Close MSIonPump    '---Close Spec/Ion Pump valve
+# Delay 3
+# Open ExtrToMS    '---Open spec/extr. line valve
+# Message "Allowing equilibration--"
+# Equil    '---Doing gas equilibration subroutine
+# '---End of equilibration, set valves for analysis
+# Close ExtrToMS             '---Close spec/Getter valve
+# Delay 2
+# Open "Inlet 1"
+# Open "Inlet 2"
+# Open "Prep IG"
+# '---Valves set for analysis, measure the gas
+# MeasureGas
+# Delay 2
+# Open MSIonPump
+# Open "MS IG"
+
+
+def main():
+    info('pump sample')    
+    open('D') # Ion pump extraction line
+    open('B') # CO2 Inlet
+    open('A') # Excimer Inlet
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT argus_pump_ms.py
+#===============================================================================
+"""
+"""
+
+# Open "MS IG"
+
+
+def main():
+    info('pump ms')
+    open(description='MSIonPump')
+    close(description='MS IG')
