1008840d5464420ce494c28f339abafed3fdaafc
Argon <jirhiker@gmail.com>
2023-09-27 11:19:34 -0500

    repo updated for analysis ba-01-A-26959

--- scripts/argus/argus_EXCIMER_blank_pump_to_IP.py
+++ scripts/argus/argus_EXCIMER_blank_pump_to_IP.py
@@ -16,7 +16,7 @@ def main():
         sleep(2)
         close('D') # Ion Pump
         sleep(2)
-        sleep(154) # Ablation time (152s) + sleep time (2s): 30 s warmup not taken into account
+        sleep(200) # Ablation time (152s) + sleep time (2s): 30 s warmup not taken into account
         #if ramp_duration>0:
         #    '''
         #    #style 1.
