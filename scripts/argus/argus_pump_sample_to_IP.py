528e9e53c5ac46bb30d3c5370c1bc94225216b09
Argon <jirhiker@gmail.com>
2024-01-25 15:22:46 -0600

    repo updated for analysis ba-01-A-27966

--- scripts/argus/argus_pump_sample_to_IP.py
+++ scripts/argus/argus_pump_sample_to_IP.py
@@ -33,6 +33,6 @@
 
 def main():
     info('pump sample')
-    close(name="B", description="CO2 Inlet")
+    open(name="B", description="CO2 Inlet")
     open(name="A", description="Excimer Inlet")
     open(name="D", description="Ion Pump")
