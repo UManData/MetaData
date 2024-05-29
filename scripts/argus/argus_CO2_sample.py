93bbaa329b96a039b0e300cbfc6568c55f69fc37
Argon <jirhiker@gmail.com>
2024-05-28 19:50:38 -0500

    repo updated for analysis ba-01-A-28882

--- scripts/argus/argus_CO2_sample.py
+++ scripts/argus/argus_CO2_sample.py
@@ -19,8 +19,8 @@ def main():
         close('D') # Ion Pump
         sleep(2)
         #sleep(240) # Getter time (180s) + ramp (20s) and heating (40s) CO2 time
-        #sleep(300) # Getter time (240) + ramp (20s) and heating (40s) CO2 time (TF mica)
-        sleep(140) # Getter time (60s) + ramp (20s) and heating (60s) CO2 time (TF Sanidine, Olduvai)
+        sleep(180) # Getter time (120) + ramp (20s) and heating (40s) CO2 time (TF mica)
+        #sleep(140) # Getter time (60s) + ramp (20s) and heating (60s) CO2 time (TF Sanidine, Olduvai)
         #sleep(80) # ramp (20s) + heating (60s) CO2 time (SH)
         close('B') # CO2 inlet
         
