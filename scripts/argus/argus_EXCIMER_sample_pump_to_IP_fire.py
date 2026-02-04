7f88bb123a91a7fbfd807922f456b64ea17b1132
Argon <jirhiker@gmail.com>
2026-02-04 15:38:05 -0600

    repo updated for analysis ba-01-A-33832

--- scripts/argus/argus_EXCIMER_sample_pump_to_IP_fire.py
+++ scripts/argus/argus_EXCIMER_sample_pump_to_IP_fire.py
@@ -49,7 +49,7 @@ def main():
     close(description='MS IG')
     sleep(cleanup)
 #===============================================================================
-# POST EQUILIBRATION SCRIPT argus_pump_sample_to_IP.py
+# POST EQUILIBRATION SCRIPT argus_pump_sample_to_turbo.py
 #===============================================================================
 """
 """
@@ -86,9 +86,12 @@ def main():
 
 def main():
     info('pump sample')
-    open(name="B", description="CO2 Inlet")
-    open(name="A", description="Excimer Inlet")
-    open(name="D", description="Ion Pump")
+    open('B')
+    #open(description='Excimer Inlet')
+    close('D')
+    open('A')
+    close(description='Prep IG')
+
 #===============================================================================
 # POST MEASUREMENT SCRIPT argus_pump_ms.py
 #===============================================================================
